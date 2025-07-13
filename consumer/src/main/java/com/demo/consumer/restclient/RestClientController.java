84de8f5347cbed4fb64158d784a4e150226947bf
Sathya <neelasathya06@gmail.com>
2025-07-13 08:52:57 -0400

    provider-consumer using restClient impl

---
+++ consumer/src/main/java/com/demo/consumer/restclient/RestClientController.java
@@ -0,0 +1,28 @@
+package com.demo.consumer.restclient;
+
+import com.demo.consumer.restTemplate.RestTemplateClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/api/rest-client")
+@RequiredArgsConstructor
+public class RestClientController {
+
+    public final ProviderRestClient providerRestClient;
+
+    @GetMapping("/instance")
+    public String getInstance(){
+//        RestClient restClient = RestClient.create();
+//        return restClient.get()
+//                .uri("http://localhost:8081/instance-info")
+//                .retrieve()
+//                .body(String.class);
+
+        return providerRestClient.getInstaceInfo();
+    }
+
+}
